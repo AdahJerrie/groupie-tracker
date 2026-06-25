256a73742d4267b5329fe4dd57c468d5a8b5cac3
jadah <adahjeremiah3@gmail.com>
2026-06-25 12:01:53 +0100

    .

--- main.go
+++ main.go
@@ -42,4 +42,28 @@ func main() {
 	for i, artist := range artists {
 		fmt.Printf("%d: %s (since %d)\n", i, artist.Name, artist.CreationDate)
 	}
+
+	relate := map[string][]string{
+		"Lagos": {"12-03-2002", "23-07-2004", "15-02-2015"},
+		"Benue": {"28-12-10-2022", "21-04-2023", "10-01-2024"},
+		"Abuja": {"20-08-2024", "11-05-2025"},
+	}
+
+	for key, value := range relate {
+		fmt.Printf("%s --> %v\n", key, value)
+	}
+
+	dates, ok := relate["Abuja"]
+	if !ok {
+		fmt.Println("Not found")
+	} else {
+		fmt.Println(dates)
+	}
+
+	date, ok := relate["Niger"]
+	if !ok {
+		fmt.Println("Not found")
+	} else {
+		fmt.Println(date)
+	}
 }
