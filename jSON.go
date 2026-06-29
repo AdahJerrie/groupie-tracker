8cf00a36c6ab156e303a5360982c6cb08f888dd3
root <root@JERRIE.localdomain>
2026-06-29 05:34:02 +0100

    unmarshaling

--- jSON.go
+++ jSON.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
 type Artist struct {
 	ID           int      `json:"id"`
 	Image        string   `json:"image"`
@@ -10,5 +16,11 @@ type Artist struct {
 }
 
 func main() {
-	if err := Unmarshal([]byte(), v any)
+	rawJSON := `{"id":1,"image":"https://example.com/queen.jpeg","name":"Queen","members":["Freddie Mercury","Brian May","Roger Taylor","John Deacon"],"creationDate":1970,"firstAlbum":"14-12-1973"}`
+	var artist Artist
+	if err := json.Unmarshal([]byte(rawJSON), &artist); err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Printf("%+v\n", artist)
 }
