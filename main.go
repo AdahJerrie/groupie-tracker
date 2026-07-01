54adc341668dfea0814dfd1380c53ec4afb2155d
jadah <adahjeremiah3@gmail.com>
2026-07-01 15:58:56 +0100

    modified: main

--- main.go
+++ main.go
@@ -31,10 +31,21 @@ func main() {
 	artistsDetails, err := FetchArtists("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
 		log.Println(err)
+		return
+	}
+
+	locateDetails, err := FetchLocations("https://groupietrackers.herokuapp.com/api/locations")
+	if err != nil {
+		log.Println(err)
+		return
 	}
 
 	for i, artist := range artistsDetails {
 		fmt.Printf("%d %+v\n", i, artist)
+	}
+
+	for i, location := range locateDetails.Index {
+		fmt.Printf("%d %s\n", i, location.Locations[0:5])
 	}
 }
 
@@ -56,4 +67,23 @@ func FetchArtists(url string) ([]Artist, error) {
 	}
 
 	return artists, nil
+}
+
+func FetchLocations(url string) (LocationIndex, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return LocationIndex{}, fmt.Errorf("fetching locations: %w", err)
+	}
+	defer resp.Body.Close()
+
+	respByte, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return LocationIndex{}, fmt.Errorf("reading response body: %w", err)
+	}
+
+	var locate LocationIndex
+	if err := json.Unmarshal(respByte, &locate); err != nil {
+		return LocationIndex{}, fmt.Errorf("decoding locate JSON: %w", err)
+	}
+	return locate, nil
 }
