8e846304c949e2b80d5ea2183b44521ba38e22dd
Adah Jerrie <adahjeremiah3@gmail.com>
2026-07-13 14:11:13 +0100

    .

--- main.go
+++ main.go
@@ -87,6 +87,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", homeHandler)
 	mux.HandleFunc("/artist", artistHandler)
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	log.Println("starting server on :8080")
 	if err := http.ListenAndServe(":8080", mux); err != nil {
